aec4620631e82fd1d41a95a4980022100230e3ba
nicolasblancom <nicolasblancom@gmail.com>
2019-02-26 01:18:02 +0100

    php 7.1

--- install.sh
+++ install.sh
@@ -16,12 +16,12 @@ sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 
 echo "Installing PHP"
-sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
+sudo apt-get install php7.1 php7.1-fpm php7.1-mysql -y
 #To resolve the issue mentioned at https://github.com/oerdnj/deb.sury.org/issues/372, I fired below command
 sudo apt-get install libpcre3
 
 echo "Installing PHP extensions"
-sudo apt-get install curl php7.0-common php7.0-cli  php7.0-curl php7.0-mcrypt php7.0-gd  -y
+sudo apt-get install curl php7.1-common php7.1-cli  php7.1-curl php7.1-mcrypt php7.1-gd  -y
 
 echo "Getting ready to install mysql 5.7"
 sudo apt-get install debconf-utils -y
